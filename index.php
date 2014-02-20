2f252bad1fcf457a26f7a576cb47a6b050018436
Harry Ramdhani <astroland.69@gmail.com>
2014-02-20 21:34:31 +0700

    update code

--- index.php
+++ index.php
@@ -3,18 +3,21 @@ $x = 12;
 $y = 16;
 
 while ($x <= $y) {
-    if($x > 0){
-    	if($x % 3 == 0){ // if the remainder of $x/3 = 0
-    		echo "Fizz"; 
-    		if($x % 5 == 0){ // if the remainder of $x/5 = 0
-	    		echo "Buzz";
-	    	}
+    
+	if($x % 3 == 0){ // if the remainder of $x/3 = 0
+		echo "Fizz"; 
+		if($x % 5 == 0){ // if the remainder of $x/5 = 0
+    		echo "Buzz";
     	}
-    	else{
-    		echo $x;
-    	}
-    	echo "<br />";
-    }
+	}
+	else if($x % 5 == 0){ // if the remainder of $x/5 = 0
+		echo "Buzz";
+	}
+	else{
+		echo $x;
+	}
+	echo "<br />";
+    
     $x++;
 };
 
